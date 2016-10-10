0d7bec72fafb67a9a54973489c0b884ee337c17f
blkshdw <aleksey.glebow@gmail.com>
2016-10-10 18:02:16 +0300

    initial commit; empty project

---
+++ Chisto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chisto
+//
+//  Created by Алексей on 10.10.16.
+//  Copyright © 2016 Binary Blitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
