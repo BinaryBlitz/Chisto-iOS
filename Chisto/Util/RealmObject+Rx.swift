6e866f93894b0c3d0d221c104dda9c555ca7e3ef
blkshdw <aleksey.glebow@gmail.com>
2016-11-18 21:54:51 +0300

    Fix code style

---
+++ Chisto/Util/RealmObject+Rx.swift
@@ -0,0 +1,15 @@
+//
+//  RealmObject+Rx.swift
+//  Chisto
+//
+//  Created by Алексей on 17.11.16.
+//  Copyright © 2016 Binary Blitz. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import RxSwift
+
+extension Object {
+  
+}
