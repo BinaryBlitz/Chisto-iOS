0756e1ec757a6c00ea7c788ca414b98eb5bba574
blkshdw <aleksey.glebow@gmail.com>
2016-11-25 03:22:51 +0300

    Add a payment screen

---
+++ Chisto/UI/PaymentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PaymentViewController.swift
+//  Chisto
+//
+//  Created by Алексей on 25.11.16.
+//  Copyright © 2016 Binary Blitz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PaymentViewController: UIViewController {
+  @IBOutlet weak var webView: UIWebView!
+  
+  override func viewDidLoad() {
+    
+  }
+  
+}
---
+++ Chisto/UI/PaymentViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentViewModel.swift
+//  Chisto
+//
+//  Created by Алексей on 25.11.16.
+//  Copyright © 2016 Binary Blitz. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class PaymentViewModel {
+  let didPressCloseButton = PublishSubject<Void>()
+  let url: URL
+  
+  init(order: Order) {
+    
+  }
+}
