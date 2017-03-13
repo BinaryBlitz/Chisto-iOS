b74662dd88caf9122d47be1a71081cb500f55ed0
blkshdw <aleksey.glebow@gmail.com>
2017-03-13 23:41:13 +0300

    Change base url

--- Chisto/Data/NetworkManager.swift
+++ Chisto/Data/NetworkManager.swift
@@ -131,7 +131,7 @@ enum NetworkError: Error, CustomStringConvertible {
 
 class NetworkManager {
 
-  let baseURL = "https://chisto.xyz"
+  let baseURL = "https://chis.to"
 
   func doRequest(method: HTTPMethod, _ path: APIPath,
                  _ params: Parameters = [:],
